51e4007358ec248da95f484e5982ba4b94021ce6
ahmady022 <ahamdi@meiladigital.com>
2022-07-15 17:31:39 +0200

    Handle more exceptions in APIExceptionFilter

--- __Common/APIExceptionFilter.cs
+++ __Common/APIExceptionFilter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
 
 namespace Common;
 
@@ -11,10 +12,28 @@ public class APIExceptionFilter : ExceptionFilterAttribute
     {
         _logger = logger;
     }
-    public override void OnException(ExceptionContext actionContext)
+    public override void OnException(ExceptionContext exceptionContext)
     {
-        var errorMessage = $"{actionContext.Exception.Message}|_|{actionContext.Exception.InnerException?.Message}|_|{actionContext.Exception.InnerException?.InnerException?.Message}";
-        _logger.LogError(errorMessage);
-        actionContext.Result = new BadRequestObjectResult(errorMessage);
+        string _errorMessage;
+        if (exceptionContext.Exception is HttpRequestException _httpException)
+        {
+            _errorMessage = _httpException.Message;
+            _logger.LogError(_errorMessage);
+            exceptionContext.Result = _httpException.StatusCode switch
+            {
+                HttpStatusCode.NotFound => new NotFoundObjectResult(_errorMessage),
+                HttpStatusCode.Unauthorized => new UnauthorizedObjectResult(_errorMessage),
+                HttpStatusCode.Conflict => new ConflictObjectResult(_errorMessage),
+                HttpStatusCode.UnprocessableEntity => new UnprocessableEntityObjectResult(_errorMessage),
+                HttpStatusCode.BadRequest => new BadRequestObjectResult(_errorMessage),
+                _ => new BadRequestObjectResult(_errorMessage),
+            };
+        }
+        else
+        {
+            _errorMessage = $"{exceptionContext.Exception.Message}|_|{exceptionContext.Exception.InnerException?.Message}|_|{exceptionContext.Exception.InnerException?.InnerException?.Message}";
+            _logger.LogError(_errorMessage);
+            exceptionContext.Result = new BadRequestObjectResult(_errorMessage);
+        }
     }
 }
