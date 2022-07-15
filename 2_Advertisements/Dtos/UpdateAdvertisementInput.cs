5714397e126d7b6e155d1aad34e17915f73b66f7
ahmady022 <ahamdi@meiladigital.com>
2022-07-15 16:23:21 +0200

    add validation annotations for Guid Ids in all UpdateInputs

--- 2_Advertisements/Dtos/UpdateAdvertisementInput.cs
+++ 2_Advertisements/Dtos/UpdateAdvertisementInput.cs
@@ -5,5 +5,9 @@ namespace Dtos;
 public class UpdateAdvertisementInput : CreateAdvertisementInput
 {
     [Required]
+    [RegularExpression(
+        @"^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$",
+        ErrorMessage = "Not a valid Id value"
+    )]
     public Guid Id { get; set; }
 }
