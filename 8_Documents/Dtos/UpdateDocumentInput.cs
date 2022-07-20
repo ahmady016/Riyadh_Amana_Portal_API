ecfa92f533b26d2ed2843b8d99aa36091a22059b
mohamedElgarhy <mohamedgamal12632@gmail.com>
2022-07-20 21:43:57 +0200

    Documents done

---
+++ 8_Documents/Dtos/UpdateDocumentInput.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dtos;
+
+public class UpdateDocumentInput : CreateDocumentInput
+{
+    [Required]
+    [RegularExpression(
+        @"^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$",
+        ErrorMessage = "Not a valid Id value"
+    )]
+    public Guid Id { get; set; }
+}
