cd557227940e889888a4b5b1320788c85855e375
Ahmed Hamdi <ahamdi@meiladigital.com>
2022-08-02 19:19:28 +0000

    Make FileUrl In ContactUs Nullable

---
+++ __DB/Migrations/20220802191815_Make_FileUrl_In_ContactUs_Nullable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace amana_mono.__DB.Migrations
+{
+    public partial class Make_FileUrl_In_ContactUs_Nullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "file_url",
+                table: "contact_us",
+                type: "varchar(400)",
+                maxLength: 400,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(400)",
+                oldMaxLength: 400);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "file_url",
+                table: "contact_us",
+                type: "varchar(400)",
+                maxLength: 400,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(400)",
+                oldMaxLength: 400,
+                oldNullable: true);
+        }
+    }
+}
