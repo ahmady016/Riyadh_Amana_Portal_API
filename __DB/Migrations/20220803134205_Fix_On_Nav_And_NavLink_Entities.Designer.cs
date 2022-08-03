9ea2a6d1840c63c4eb64ca2ad1478a94481a9aa1
Ameera jabban <Ameera.a.jabban@hotmail.com>
2022-08-03 16:43:00 +0300

    Add migration

--- __DB/Migrations/20220803134205_Fix_On_Nav_And_NavLink_Entities.Designer.cs
+++ __DB/Migrations/20220803134205_Fix_On_Nav_And_NavLink_Entities.Designer.cs
@@ -4,6 +4,7 @@ using DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace amana_mono.__DB.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220803134205_Fix_On_Nav_And_NavLink_Entities")]
+    partial class Fix_On_Nav_And_NavLink_Entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
